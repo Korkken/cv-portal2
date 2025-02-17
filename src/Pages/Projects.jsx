6aa587c23f846ce69842228ace19d925fe4dc5cc
Korkken <Korkken@gmail.com>
2025-02-17 15:54:41 +0100

    test

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -17,7 +17,17 @@ useEffect(()=>{
     .catch((error)=> {
         console.log(error)
     })
-}, [])
+}, []);
+(function(l) {
+    if (l.search[1] === '/' ) {
+      var decoded = l.search.slice(1).split('&').map(function(s) { 
+        return s.replace(/~and~/g, '&')
+      }).join('?');
+      window.history.replaceState(null, null,
+          l.pathname.slice(0, -1) + decoded + l.hash
+      );
+    }
+  }(window.location));
 return (
     <>
         <main>
